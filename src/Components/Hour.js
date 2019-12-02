c99002754499e5cb299de5d8af1128cf75a0bc16
nickmilanos <nickmilanos@hotmail.com>
2019-12-02 13:26:16 +0200

    Improved view of hours,minutes and seconds

--- src/Components/Hour.js
+++ src/Components/Hour.js
@@ -14,25 +14,35 @@ export default class Hour extends React.Component{
         setInterval(() => {
             let date = new Date();
             let updatedSeconds = date.getSeconds();
-            this.setState({
-                seconds: updatedSeconds
-            });
+            if(updatedSeconds < 10){
+                this.setState({
+                    seconds: `0${updatedSeconds}`
+                });
+            }
+            else{
+                this.setState({
+                    seconds: updatedSeconds
+                });
+            }
         }, 1000);
     }
+
 
     componentDidMount(){
         this.handleSeconds();
     }
 
     render(){
-        let date = new Date();
         const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        let currentMonth = months[date.getMonth()];
-        let currentDay = days[date.getDay()];
+        const date = new Date();
+        const currentMonth = months[date.getMonth()];
+        const currentDay = days[date.getDay()];
+        let currentHour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
+        let currentMinute = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
         return(
             <div id="hourContainer">
-                <h1>{date.getHours()}:{date.getMinutes()}:{this.state.seconds}</h1>
+                <h1>{currentHour}:{currentMinute}:{this.state.seconds}</h1>
                 <h2>{currentDay}, {date.getDate()} {currentMonth} {date.getFullYear()}</h2>
             </div>
         );

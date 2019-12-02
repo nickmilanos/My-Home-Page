8370b2d4c219ce1b741ffa527f123d973cb18b2f
nickmilanos <nickmilanos@hotmail.com>
2019-12-02 15:30:19 +0200

    Added main weather and location info

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -5,9 +5,14 @@ export default class Weather extends React.Component{
         super();
         this.state = {
             lon: "",
-            lat: ""
+            lat: "",
+            cityName: '',
+            country: "",
+            temp: "",
+            humidity: ""
         };
         this.success = this.success.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
     }
     
     success(position){
@@ -20,13 +25,27 @@ export default class Weather extends React.Component{
 
     componentDidMount(){
         navigator.geolocation.getCurrentPosition(this.success);
+        fetch(`http://api.openweathermap.org/data/2.5/weather?id=256429&units=metric&appid=9b00c332e03384ca3992818f17135f63`)
+            .then(res => res.json())
+            .then(res => {console.log(res)
+                this.setState({
+                    cityName: res.name,
+                    country: res.sys.country,
+                    temp: res.main.temp,
+                    humidity: res.main.humidity
+                });
+            });
     }
+
 
     render(){
         return(
             <div id="watherContainer">
-                <h2>{this.state.lon}</h2>
-                <h2>{this.state.lat}</h2>
+                <h2>Longitude: {this.state.lon}</h2>
+                <h2>Latitude: {this.state.lat}</h2>
+                <h2>{this.state.cityName}, {this.state.country}</h2>
+                <h2>Temperature {this.state.temp} °C</h2>
+                <h2>Humidity: {this.state.humidity}%</h2>
             </div>
         );
     }

78ad64f195ff68340865dcb8e0edf4ce7c83980c
nickmilanos <nickmilanos@hotmail.com>
2020-01-18 15:14:29 +0200

    Added functionality for generating random wallpaper from target/favorite collections of unsplash

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,7 +5,7 @@ import Weather from './Weather.js';
 import GoogleSearch from './GoogleSearch.js';
 
 export default class App extends React.Component {
-    getWallpaper(){
+    getRandomWallpaperFromUnsplash(){
         let myRoot = document.getElementById("root");
         const accessKey = "973a2dc62497213188e486e906dc5128f123552910555beecdf9894d63b29bb0";
         fetch(`https://api.unsplash.com/photos/random?client_id=${accessKey}`, {
@@ -20,8 +20,36 @@ export default class App extends React.Component {
                 myRoot.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${randomImage})`;
             });
     }
+
+    getRandomWallpaperFromCollectionOfUnsplash = () => {
+        let myRoot = document.getElementById("root");
+        const favoriteCollections = {
+            patternsAndTextures: "175083",
+            animals: "181581",
+            intoTheWild: "225",
+            raindropsGlass: "1410320",
+            landscape: "827743",
+            earthPlanets: "894"
+        };
+        let favoriteCollectionsValues = Object.values(favoriteCollections);
+        let randomCollectionId = favoriteCollectionsValues[Math.floor(Math.random() * favoriteCollectionsValues.length)];
+        for(let key of Object.keys(favoriteCollections)){
+            if(favoriteCollections[key] === randomCollectionId) console.log(key);
+        }
+        const accessKey = "973a2dc62497213188e486e906dc5128f123552910555beecdf9894d63b29bb0";
+        fetch(`https://api.unsplash.com/collections/${randomCollectionId}/photos?client_id=${accessKey}`, {
+            "Accept-Version": "v1"
+        })
+            .then(res => res.json())
+            .then(data => {
+                let randomImage = data[Math.floor(Math.random() * data.length)].urls.raw;
+                console.log(`Images in Collection: ${data.length}`);
+                myRoot.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${randomImage})`;
+            });
+    }
+
     render() {
-        this.getWallpaper();
+        this.getRandomWallpaperFromCollectionOfUnsplash();
         return ( 
         <div id = "appContainer" >
             <Hour />

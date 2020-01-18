169e64591420f66d2443582cda006dea6373c7f1
nickmilanos <nickmilanos@hotmail.com>
2020-01-18 13:25:39 +0200

    Random Background Image generated through unsplash API

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,7 +5,23 @@ import Weather from './Weather.js';
 import GoogleSearch from './GoogleSearch.js';
 
 export default class App extends React.Component {
+    getWallpaper(){
+        let myRoot = document.getElementById("root");
+        const accessKey = "973a2dc62497213188e486e906dc5128f123552910555beecdf9894d63b29bb0";
+        fetch(`https://api.unsplash.com/photos/random?client_id=${accessKey}`, {
+            headers: {
+                "Accept-Version": "v1"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                let randomImage = data.urls.raw;
+                console.log(randomImage);
+                myRoot.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${randomImage})`;
+            });
+    }
     render() {
+        this.getWallpaper();
         return ( 
         <div id = "appContainer" >
             <Hour />
--- src/Components/GoogleSearch.js
+++ src/Components/GoogleSearch.js
@@ -5,7 +5,7 @@ export default class GoogleSearch extends React.Component{
         return(
             <div id="googleSearchContainer">
                 <form method="get" action="https://www.google.com/search">
-                <input type="text" name="q" id="search" autocomplete="off" placeholder="Google Search" />
+                    <input type="text" name="q" id="search" autocomplete="off" placeholder="Google Search" />
                 </form>
             </div>
         );
